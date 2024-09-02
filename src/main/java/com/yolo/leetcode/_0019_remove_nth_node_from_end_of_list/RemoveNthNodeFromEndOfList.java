d9685037112e3eaed1af7a065a04e026aa171ab4
yolo <919616056@qq.com>
2024-09-02 22:24:49 +0800

    0019 删除链表的倒数第N个结点

---
+++ src/main/java/com/yolo/leetcode/_0019_remove_nth_node_from_end_of_list/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,45 @@
+package com.yolo.leetcode._0019_remove_nth_node_from_end_of_list;
+
+public class RemoveNthNodeFromEndOfList {
+
+
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    class Solution {
+        public ListNode removeNthFromEnd(ListNode head, int n) {
+            ListNode dummy = new ListNode(0, head);
+            ListNode first = head;
+            ListNode second = dummy;
+
+            for (int i = 0; i < n; i++) {
+                first = first.next;
+            }
+            while (first != null) {
+                first = first.next;
+                second = second.next;
+            }
+            second.next = second.next.next;
+            return dummy.next;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
