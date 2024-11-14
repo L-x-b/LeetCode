bd9385b0e0265f9b7a54ab24df4f95813f27e86b
yolo <919616056@qq.com>
2024-11-14 16:46:09 +0800

    0108 将有序数组转换成二叉树搜索树

---
+++ src/main/java/com/yolo/leetcode/_0108_convert_sorted_array_to_binary_search_tree/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,46 @@
+package com.yolo.leetcode._0108_convert_sorted_array_to_binary_search_tree;
+
+
+import java.util.Random;
+
+public class ConvertSortedArrayToBinarySearchTree {
+
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        Random rand = new Random();
+
+        public TreeNode sortedArrayToBST(int[] nums) {
+            return helper(nums, 0, nums.length - 1);
+        }
+
+        public TreeNode helper(int[] nums, int left, int right) {
+            if (left > right) return null;
+
+            int mid = (left + right + rand.nextInt(2)) / 2;
+            TreeNode node = new TreeNode(nums[mid]);
+
+            node.left = helper(nums, left, mid - 1);
+            node.right = helper(nums, mid + 1, right);
+
+            return node;
+        }
+    }
+}
