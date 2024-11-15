cbe769eb40fc1ec9e453a479161b80e3e7deff25
yolo <919616056@qq.com>
2024-11-15 09:37:19 +0800

    0098 验证二叉搜索树

---
+++ src/main/java/com/yolo/leetcode/_0098_validate_binary_search_tree/ValidateBinarySearchTree.java
@@ -0,0 +1,43 @@
+package com.yolo.leetcode._0098_validate_binary_search_tree;
+
+public class ValidateBinarySearchTree {
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    class Solution {
+        public boolean isValidBST(TreeNode root) {
+            return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+        }
+
+        public boolean isValidBST(TreeNode node, long lower, long upper) {
+            if (node == null) {
+                return true;
+            }
+            if (node.val <= lower || node.val >= upper) {
+                return false;
+            }
+            return isValidBST(node.left, lower, node.val) && isValidBST(node.right, node.val, upper);
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
